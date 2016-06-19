788eda4bd30210e64505e9aff362ceab0034fd2c
Danny <arntz.D@gmail.com>
2016-06-19 10:58:36 +0200

    Changed gA send event

--- app/assets/js/component/_tracking.js
+++ app/assets/js/component/_tracking.js
@@ -4,28 +4,19 @@
 // track in google analytics
 function trackView(url) {
     url = url.replace('https://', '').replace('http://', '').replace('mailto:', '');
-    ga('send', {
-		  hitType: 'pageview',
-		  page: '/' + url
-		});
-
     ga('send', 'pageview', '/' + url);
 }
 
 // track events in google analytics
 // example: category(video), action(play,pause,stop), label(title, name file), value(length, dollar value), nonInteraction(false,true (default is false and it impacts the bounce rate calculations))
 function trackEvent(category, action, label) {
-	ga('send', {
-		hitType: 'event',
-		eventCategory: category,
-		eventAction: action,
-		eventLabel: label
-	});
+	ga('send', 'event', category, action, label);
 
 }
 
 // track click event
 $('[data-event="ga"]').on('click', function() {
+
  var category = $(this).data('category') ? $(this).data('category') : '',
      action = $(this).data('action') ? $(this).data('action') : '',
      label = $(this).data('label') ? $(this).data('label') : '';
@@ -49,6 +40,7 @@ $('body').on('click', 'a[target="_blank"], [data-ga]', function() {
 	} else if (link.attr('target')) {
 		value = 'external/' + href;
 	}
+	
 	trackView(value);
 
 });
