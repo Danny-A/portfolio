df4baddb0bf474a893f1cafce73a3232fab9d0ea
Danny Arntz <danny.arntz@mangrove.nl>
2017-03-06 08:09:13 +0100

    feat(config): add dev database environment

--- craft/config/db.php
+++ craft/config/db.php
@@ -8,20 +8,39 @@
  */
 
 return array(
+	'*' => array(
+		// The database server name or IP address. Usually this is 'localhost' or '127.0.0.1'.
+		'server' => 'localhost',
 
-	// The database server name or IP address. Usually this is 'localhost' or '127.0.0.1'.
-	'server' => 'localhost',
+		// The name of the database to select.
+		'database' => 'dannya1q_craft_prod',
 
-	// The name of the database to select.
-	'database' => 'dannya1q_craft_prod',
+		// The database username to connect with.
+		'user' => 'dannya1q_live',
 
-	// The database username to connect with.
-	'user' => 'dannya1q_live',
+		// The database password to connect with.
+		'password' => '3110d3sign&Crime',
 
-	// The database password to connect with.
-	'password' => '3110d3sign&Crime',
+		// The prefix to use when naming tables. This can be no more than 5 characters.
+		'tablePrefix' => 'craft',
+	),
 
-	// The prefix to use when naming tables. This can be no more than 5 characters.
-	'tablePrefix' => 'craft',
+	'.dev' => array(
+		// The database server name or IP address. Usually this is 'localhost' or '127.0.0.1'.
+		'server' => 'localhost',
+
+		// The name of the database to select.
+		'database' => 'portfolio',
+
+		// The database username to connect with.
+		'user' => 'root',
+
+		// The database password to connect with.
+		'password' => '',
+
+		// The prefix to use when naming tables. This can be no more than 5 characters.
+		'tablePrefix' => 'craft',
+
+	)
 
 );
