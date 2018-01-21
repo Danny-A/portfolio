40c676d58afeba66716db6b75aab180d9ae7d582
Danny Arntz <arntz.D@gmail.com>
2018-01-21 18:11:20 +0100

    chore(config): add config files etc

--- config/db.php
+++ config/db.php
@@ -14,5 +14,5 @@ return [
     'database' => getenv('DB_DATABASE'),
     'schema' => getenv('DB_SCHEMA'),
     'tablePrefix' => getenv('DB_TABLE_PREFIX'),
-    'port' => getenv('DB_PORT')
+    'port' => getenv('DB_PORT'),
 ];
--- config/general.php
+++ config/general.php
@@ -6,11 +6,20 @@
  * list of the available settings in vendor/craftcms/cms/src/config/GeneralConfig.php.
  */
 
+define('URI_SCHEME', (isset($_SERVER['HTTPS'])) ? "https://" : "http://");
+define('SITE_URL', URI_SCHEME . $_SERVER['SERVER_NAME']);
+
 return [
     // Global settings
     '*' => [
+        // Base site URL
+        'siteUrl' => SITE_URL,
+        
         // Default Week Start Day (0 = Sunday, 1 = Monday...)
-        'defaultWeekStartDay' => 0,
+        'defaultWeekStartDay' => 1,
+
+        // Force Secure Cookies
+        'useSecureCookies' => true,
 
         // Enable CSRF Protection (recommended, will be enabled by default in Craft 3)
         'enableCsrfProtection' => true,
@@ -27,9 +36,6 @@ return [
 
     // Dev environment settings
     'dev' => [
-        // Base site URL
-        'siteUrl' => null,
-
         // Dev Mode (see https://craftcms.com/support/dev-mode)
         'devMode' => true,
     ],
