b7ad2f9edd34966151fea6298b5a16b354cbe8b6
Danny Arntz <arntz.D@gmail.com>
2018-01-22 22:26:26 +0100

    Removed Craft, added React

--- gulpfile.js
+++
@@ -1,6 +0,0 @@
-var gulp = require('gulp');
-var requireDir = require('require-dir');
-var config = require('./gulp/config');
-
-// Require all tasks in gulp/tasks, including subfolders
-requireDir('./gulp/tasks', { recurse: true });
