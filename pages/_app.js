a63a015315faec3fd62759fa961cdd35fdb53215
Danny Arntz <arntz.D@gmail.com>
2020-12-02 09:21:30 +0100

    Fix analytics tag

--- pages/_app.js
+++ pages/_app.js
@@ -70,16 +70,20 @@ function MyApp({ Component, pageProps }) {
 
         <link rel="manifest" href="manifest.json" />
         <meta name="theme-color" content="#000000" />
-      </Head>
-      <script>
-        (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-        (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-        m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-        })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+        {/* -- Global site tag (gtag.js) - Google Analytics -- */}
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-G9RW3D6T9K"></script>
+        <script 
+          dangerouslySetInnerHTML={{
+            __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
 
-        ga('create', 'UA-6896893-6', 'auto');
-        ga('send', 'pageview');
-      </script>
+            gtag('config', 'G-G9RW3D6T9K');
+          `
+          }}
+        />
+      </Head>
       <nav className="navigation" role="navigation">
         <Link href="/">
           About
