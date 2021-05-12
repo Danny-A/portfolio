4ed589d3b74f7b09fa1245429a299aee64ff1f55
Danny Arntz <arntz.D@gmail.com>
2021-05-12 09:14:56 +0200

    Split document and add improved GA

---
+++ pages/_document.js
@@ -0,0 +1,99 @@
+import Document, { Html, Head, Main, NextScript } from 'next/document'
+import { GA_TRACKING_ID } from "../utils/gtag";
+
+class MyDocument extends Document {
+  static async getInitialProps(ctx) {
+    const initialProps = await Document.getInitialProps(ctx)
+    return { ...initialProps }
+  }
+
+  render() {
+    return (
+      <Html>
+      <Head>
+        <meta charSet="utf-8" />
+        <meta httpEquiv="x-ua-compatible" content="ie=edge" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <base href="https://www.dannyarntz.nl/" />
+        <title>Danny Arntz - Front-end developer, Utrecht, Rotterdam en Amsterdam</title>
+
+        {/* -- Standard SEO -- */}
+
+        <meta httpEquiv="Content-Type" content="text/html; charset=utf-8" />
+        <meta name="referrer" content="no-referrer-when-downgrade" />
+        <meta name="description" content="Senior Front-end developer uit Utrecht. JavaScript(ES6+), ReactJS, HTML en CSS ervaring. Focus op kwaliteit en breng projecten en teams naar een hoger niveau." />
+        <meta name="generator" content="SEOmatic" />
+        <link rel="alternate" href="https://www.dannyarntz.nl" hrefLang="x-default" />
+        <meta name="geo.placename" content="Danny Arntz" />
+
+        {/* -- Dublin Core basic info -- */}
+
+        <meta name="dcterms.Identifier" content="https://www.dannyarntz.nl" />
+        <meta name="dcterms.Format" content="text/html" />
+        <meta name="dcterms.Relation" content="Danny Arntz" />
+        <meta name="dcterms.Language" content="en" />
+        <meta name="dcterms.Publisher" content="Danny Arntz" />
+        <meta name="dcterms.Type" content="text/html" />
+        <meta name="dcterms.Coverage" content="https://www.dannyarntz.nl/" />
+        <meta name="dcterms.Rights" content="Copyright &copy;2020 Danny Arntz." />
+        <meta name="dcterms.Title" content="Front-end developer, Utrecht en Amsterdam" />
+        <meta name="dcterms.Subject" content="" />
+        <meta name="dcterms.Contributor" content="Danny Arntz" />
+        <meta name="dcterms.Date" content="2020-11-01" />
+        <meta name="dcterms.Description" content="Senior Front-end developer uit Utrecht. JavaScript(ES6+), ReactJS, HTML en CSS ervaring. Focus op kwaliteit en breng projecten en teams naar een hoger niveau." />
+
+        {/* -- Facebook OpenGraph -- */}
+        <meta property="og:type" content="website" />
+        <meta property="og:locale" content="en_us" />
+        <meta property="og:url" content="https://www.dannyarntz.nl" />
+        <meta property="og:title" content="Front-end developer, Utrecht en Amsterdam - Danny Arntz" />
+        <meta property="og:description" content="Senior Front-end developer uit Utrecht. JavaScript(ES6+), ReactJS, HTML en CSS ervaring. Focus op kwaliteit en breng projecten en teams naar een hoger niveau." />
+        <meta property="og:image" content="https://www.dannyarntz.nl/images/share-image.png" />
+        <meta property="og:image:type" content="image/png" />
+        <meta property="og:image:width" content="1200" />
+        <meta property="og:image:height" content="630" />
+        <meta property="og:site_name" content="Danny Arntz" />
+        <meta property="og:see_also" content="https://twitter.com/wildestbunch" />
+        <meta property="og:see_also" content="https://github.com/Danny-A" />
+
+        {/* -- Twitter Card -- */}
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:site" content="@wildestbunch" />
+        <meta name="twitter:creator" content="@wildestbunch" />
+        <meta name="twitter:title" content="Front-end developer, Utrecht en Amsterdam - Danny Arntz" />
+        <meta name="twitter:description" content="Senior Front-end developer uit Utrecht. JavaScript(ES6+), ReactJS, HTML en CSS ervaring. Focus op kwaliteit en breng projecten en teams naar een hoger niveau." />
+        <meta name="twitter:image" content="https://www.dannyarntz.nl/images/share-image.png" />
+
+        {/* -- For IE 11, Chrome, Firefox, Safari, Opera -- */}
+        <link rel="icon" href="images/favicon/favicon-16.png" sizes="16x16" type="image/png" />
+        <link rel="icon" href="images/favicon/favicon-32.png" sizes="32x32" type="image/png" />
+        <link rel="icon" href="images/favicon/favicon-48.png" sizes="48x48" type="image/png" />
+        <link rel="icon" href="images/favicon/favicon-62.png" sizes="62x62" type="image/png" />
+        <link rel="icon" href="/favicon.ico" />
+
+        <link rel="manifest" href="manifest.json" />
+        <meta name="theme-color" content="#000000" />
+        {/* -- Global site tag (gtag.js) - Google Analytics -- */}
+        <script async src={`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`}></script>
+        <script 
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+            
+              gtag('config', '${GA_TRACKING_ID}');
+            `
+          }}
+        />
+      </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    )
+  }
+}
+
+export default MyDocument
--- pages/index.js
+++ pages/index.js
@@ -1,28 +1,19 @@
-import Head from 'next/head'
-
 export default function Home() {
   return (
-    <>
-      <Head>
-        <title>Danny Arntz: Front-end developer, Utrecht, Rotterdam en Amsterdam</title>
-        <meta name="description" content="Senior Front-end developer uit Utrecht. JavaScript(ES6+), ReactJS, HTML en CSS ervaring. Focus op kwaliteit en breng projecten en teams naar een hoger niveau." />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-      <div className="section">
-          <div className="grid">
-              <h2 className="meta">
-                  Front-end developer
-              </h2>
-              <h1 className="title">
-                  Danny Arntz
-              </h1>
+    <div className="section">
+        <div className="grid">
+            <h2 className="meta">
+                Front-end developer
+            </h2>
+            <h1 className="title">
+                Danny Arntz
+            </h1>
 
-              <div className="text">
-                  Hey! Ik ben Danny Arntz, een senior Front-end developer uit Utrecht. Sinds january 202 werk ik bij de start-up Adelee in Rotterdam.Als developer houd ik mij bezig met het ontwikkelen van snelle, slimme, gebruiksvriendelijke en mooie websites en web-applicaties. Dit doe ik door  altijd op zoek te zijn naar nieuwe technieken om zo de prestaties van wat ik bouw optimaal te maken. Nieuwe technieken zijn erg leuk, maar ook voor gebruikers op oudere devices weet ik alles netjes en werkend te krijgen.Werken aan animaties en transities vind ik erg gaaf. Als creatieve Front-end developer denk ik dan ook graag mee in het proces. 
-                  Wat ik belangrijk vind? Snelheid, kwaliteit, consistentie(van zowel code als visuele aspecten) en gebruiksgemak.De afgelopen jaren heb ik voor grote en kleine klanten en projecten gewerkt. Alles over meerdere devices, platforms en responsive. Hierdoor heb ik met allerlei technieken gewerkt, in een boel verschillende teams met eigen workflows. Ik werk snel, georganiseerd, consistent met BEM of andere methodes en weet tools als Webpack en Gulp goed in te zetten. Git, Gitflow en Scrummen zijn mij goed bekend.Waar ben ik goed in: JavaScript(ES6+), ReactJS, CSS3, HTML5, UX.
-              </div>
-          </div>
-      </div>
-    </>
+            <div className="text">
+                Hey! Ik ben Danny Arntz, een senior Front-end developer uit Utrecht. Sinds january 202 werk ik bij de start-up Adelee in Rotterdam.Als developer houd ik mij bezig met het ontwikkelen van snelle, slimme, gebruiksvriendelijke en mooie websites en web-applicaties. Dit doe ik door  altijd op zoek te zijn naar nieuwe technieken om zo de prestaties van wat ik bouw optimaal te maken. Nieuwe technieken zijn erg leuk, maar ook voor gebruikers op oudere devices weet ik alles netjes en werkend te krijgen.Werken aan animaties en transities vind ik erg gaaf. Als creatieve Front-end developer denk ik dan ook graag mee in het proces. 
+                Wat ik belangrijk vind? Snelheid, kwaliteit, consistentie(van zowel code als visuele aspecten) en gebruiksgemak.De afgelopen jaren heb ik voor grote en kleine klanten en projecten gewerkt. Alles over meerdere devices, platforms en responsive. Hierdoor heb ik met allerlei technieken gewerkt, in een boel verschillende teams met eigen workflows. Ik werk snel, georganiseerd, consistent met BEM of andere methodes en weet tools als Webpack en Gulp goed in te zetten. Git, Gitflow en Scrummen zijn mij goed bekend.Waar ben ik goed in: JavaScript(ES6+), ReactJS, CSS3, HTML5, UX.
+            </div>
+        </div>
+    </div>
   )
 }
