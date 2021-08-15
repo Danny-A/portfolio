35f69c5ef697e1e142d1781b59b4a5462a5a65b4
Danny Arntz <arntz.D@gmail.com>
2021-08-15 22:00:02 +0200

    Change filename CV

--- pages/index.js
+++ pages/index.js
@@ -15,7 +15,7 @@ export default function Home() {
               <h2>
                 Front-end developer
               </h2>
-              <Link href="/public/files/cv-dannyarntz.pdf">
+              <Link href="/files/cv-danny-arntz.pdf">
                 Resume ↓
               </Link>
             </div>
