c77c2f9c4934514a67e0e134fe772109423b370d
Danny Arntz <arntz.d@gmail.com>
2022-05-08 14:25:58 +0200

    Update Next, React, prepare for TS

---
+++ .eslintrc.js
@@ -0,0 +1,16 @@
+module.exports = {
+  root: true,
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 2018,
+    sourceType: 'module',
+  },
+  ignorePatterns: ['**/*'],
+  plugins: ['@typescript-eslint', 'import',],
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/eslint-recommended',
+    'plugin:@typescript-eslint/recommended',
+    'prettier',
+  ],
+};
