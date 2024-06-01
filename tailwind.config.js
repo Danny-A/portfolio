1bc58d048c8764d32808e71d5e8fe5499d326427
Danny Arntz <arntz.D@gmail.com>
2024-06-01 21:33:41 +0200

    extend tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,17 +2,17 @@
 module.exports = {
   content: ['./app/**/*.{js,ts,jsx,tsx,mdx}', './components/**/*.{js,ts,jsx,tsx,mdx}'],
   theme: {
-    colors: {
-      'primary-400': '#f08080',
-      'primary-700': '#ff4500',
-      secondary: '#7e5bef',
-      'green-400': '#41985b',
-      'green-950': '#132411',
-      'gray-100': '#dcdcdc',
-      'gray-200': '#848484',
-      'gray-950': '#121212',
-    },
     extend: {
+      colors: {
+        'primary-400': '#f08080',
+        'primary-700': '#ff4500',
+        secondary: '#7e5bef',
+        'green-400': '#41985b',
+        'green-950': '#132411',
+        'gray-100': '#dcdcdc',
+        'gray-200': '#848484',
+        'gray-950': '#121212',
+      },
       fontFamily: {
         moderat: ['Moderat', 'sans-serif'],
       },
